f9270ee69cea7e6370654cda3b5fa321c6d1ea83
pci <aswq233333@naver.com>
2023-10-24 15:04:12 +0900

    git test

--- src/main/kotlin/com/example/jpa/dto/BoardDTO.kt
+++ src/main/kotlin/com/example/jpa/dto/BoardDTO.kt
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotNull
 //@NotNull => null을 허용하지 않는다. "", " "는 허용한다.
 //@NotEmpty => null, ""을 허용하지 않는다. " "는 허용한다.
 data class BoardDTO (
+
     var bono: Int?,
 
     @field:NotEmpty(message = "dto 필수 데이터 누락")
